8abfe5bf05366a485963f2789011f72908610b13
Emily <vcs@emily.moe>
2024-10-28 17:09:54 +0000

    libcdio-paranoia: include 10.2+ prefix in `version`

--- pkgs/by-name/li/libcdio-paranoia/package.nix
+++ pkgs/by-name/li/libcdio-paranoia/package.nix
@@ -9,12 +9,12 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libcdio-paranoia";
-  version = "2.0.2";
+  version = "10.2+2.0.2";
 
   src = fetchFromGitHub {
     owner = "rocky";
     repo = "libcdio-paranoia";
-    rev = "release-10.2+${finalAttrs.version}";
+    rev = "release-${finalAttrs.version}";
     hash = "sha256-n05PSVgh6z7BFPq4CjJa5DqCO7Huj8Bsg0x3HQPsbeI=";
   };
 
