bffff41894717099d99d4b73fc4e6e35204d25e2
Emily <vcs@emily.moe>
2024-10-28 17:04:36 +0000

    libcdio-paranoia: clean up

--- pkgs/by-name/li/libcdio-paranoia/package.nix
+++ pkgs/by-name/li/libcdio-paranoia/package.nix
@@ -3,18 +3,18 @@
   stdenv,
   fetchFromGitHub,
   autoreconfHook,
-  libcdio,
   pkg-config,
+  libcdio,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "libcdio-paranoia";
   version = "2.0.2";
 
   src = fetchFromGitHub {
     owner = "rocky";
     repo = "libcdio-paranoia";
-    rev = "release-10.2+${version}";
+    rev = "release-10.2+${finalAttrs.version}";
     hash = "sha256-n05PSVgh6z7BFPq4CjJa5DqCO7Huj8Bsg0x3HQPsbeI=";
   };
 
@@ -22,20 +22,21 @@ stdenv.mkDerivation rec {
     autoreconfHook
     pkg-config
   ];
+
   buildInputs = [ libcdio ];
 
   configureFlags = lib.optionals stdenv.hostPlatform.isDarwin [ "--disable-ld-version-script" ];
 
-  meta = with lib; {
+  meta = {
     description = "CD paranoia on top of libcdio";
     longDescription = ''
       This is a port of xiph.org's cdda paranoia to use libcdio for CDROM
       access. By doing this, cdparanoia runs on platforms other than GNU/Linux.
     '';
     homepage = "https://github.com/rocky/libcdio-paranoia";
-    license = licenses.gpl3;
+    license = lib.licenses.gpl3Plus;
     maintainers = [ ];
     mainProgram = "cd-paranoia";
-    platforms = platforms.unix;
+    platforms = lib.platforms.unix;
   };
-}
+})
