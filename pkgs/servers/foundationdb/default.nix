ea9291f0063fbf18e856dc8987f93915502b27be
Emily <vcs@emily.moe>
2024-11-28 07:55:43 +0000

    foundationdb71: 7.1.32 -> 7.1.60

--- pkgs/servers/foundationdb/default.nix
+++ pkgs/servers/foundationdb/default.nix
@@ -9,8 +9,8 @@ let
   cmakeBuild = import ./cmake.nix args;
 in {
   foundationdb71 = cmakeBuild {
-    version = "7.1.32";
-    hash    = "sha256-CNJ4w1ECadj2KtcfbBPBQpXQeq9BAiw54hUgRTWPFzY=";
+    version = "7.1.60";
+    hash    = "sha256-rBksJ/prkDb+vpEkTLXOeASQKjOOt8iB8JrumnxTayo=";
     boost   = boost178;
     ssl     = openssl;
 
