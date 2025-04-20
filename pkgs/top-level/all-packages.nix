4e7602bddfb2380984822f0ac0fc50a1b657489c
Emily <vcs@emily.moe>
2025-04-20 17:01:02 +0100

    smlnj: remove obsolete Darwin patch
    That code path is only used for 32‐bit Darwin.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6546,9 +6546,7 @@ with pkgs;
   };
   # smlnjBootstrap should be redundant, now that smlnj works on Darwin natively
   smlnjBootstrap = callPackage ../development/compilers/smlnj/bootstrap.nix { };
-  smlnj = callPackage ../development/compilers/smlnj {
-    inherit (darwin) Libsystem;
-  };
+  smlnj = callPackage ../development/compilers/smlnj { };
 
   sqlx-cli = callPackage ../development/tools/rust/sqlx-cli { };
 
